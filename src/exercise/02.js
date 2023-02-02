3c15b7af494f445f339ac67454f568cb65c8fff0
Vee Uye <vee.uye@gmail.com>
2023-02-02 13:53:56 +0000

    useState: flexible localStorage Hook

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -2,18 +2,36 @@
 // http://localhost:3000/isolated/exercise/02.js
 
 import * as React from 'react'
-import {useEffect, useState} from 'react'
+import {useEffect, useRef, useState} from 'react'
 
-const useLocalStorageState = (key, initialValue = '') => {
-  const getInitialValue = () => {
-    return window.localStorage.getItem(key) ?? initialValue
-  }
-  const [state, setState] = useState(getInitialValue)
+const useLocalStorageState = (key, initialValue = '', {
+  serialize = JSON.stringify,
+  deserialize = JSON.parse,
+} = {},) =>
+ {
+  const [state, setState] = useState(() => {
+    const valueInLocalStorage = window.localStorage.getItem(key)
 
+    if (valueInLocalStorage) {
+      return deserialize(valueInLocalStorage)
+    }
+    // if the default value is computationally expensive, we can use the following to
+    // conditionally call or return it
+    return typeof initialValue === 'function' ? initialValue() : initialValue
+  })
+
+   // useRef within useEffect enables us to deal with cases where the key changes between renders.
+
+   const prevKeyRef = useRef(key)
 
   useEffect(() => {
-    window.localStorage.setItem(key, state)
-  }, [key, state])
+    const prevKey = prevKeyRef.current
+    if (prevKey !== key) {
+      window.localStorage.removeItem(prevKey)
+    }
+    prevKeyRef.current = key
+    window.localStorage.setItem(key, serialize(state))
+  }, [key, serialize, state])
 
   return [state, setState]
 }
