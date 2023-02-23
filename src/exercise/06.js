38616aeb2e770dd963396a0b927d8dd7433a92b1
Vee Uye <vee.uye@gmail.com>
2023-02-23 12:40:36 +0000

    useEffect: HTTP requests - handle errors

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -5,19 +5,33 @@ import * as React from 'react'
 import {PokemonForm, fetchPokemon, PokemonInfoFallback, PokemonDataView} from '../pokemon'
 import {useEffect, useState} from 'react'
 
+function Alert({error}) {
+    return (
+        <div role="alert">
+            There was an error: <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
+        </div>
+    )
+}
 function PokemonInfo({pokemonName}) {
 
     const [pokemon, setPokemon] = useState(null)
+    const [error, setError] = useState(null)
 
     useEffect(() => {
             if (!pokemonName) {
                 return
             }
             setPokemon(null)
-        fetchPokemon(pokemonName).then(
-            pokemonData => {
-                setPokemon(pokemonData)
-            })}, [pokemonName])
+            setError(null)
+        fetchPokemon(pokemonName)
+            .then(pokemonData => setPokemon(pokemonData))
+            .catch(error => setError(error))
+
+    }, [pokemonName])
+
+    if(error) {
+        return <Alert error={error}/>
+    }
 
     if (!pokemonName) {
         return 'Submit a pokemon'
